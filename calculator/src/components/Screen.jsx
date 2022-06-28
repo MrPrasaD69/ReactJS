7311dd930f54248fa48d77244ecca688bf3c52f5
Prasad Waghmare <prasadwaghmare6@gmail.com>
2022-06-28 18:02:40 +0530

    Simple Calculator App

---
+++ calculator/src/components/Screen.jsx
@@ -0,0 +1,11 @@
+import { Textfit } from "react-textfit";
+
+const Screen = ({ value }) => {
+  return (
+    <Textfit className="screen" mode="single" max={70}>
+      {value}
+    </Textfit>
+  );
+};
+
+export default Screen;
---
+++ calculator/src/components/Wrapper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Wrapper = ({ children }) => {
+  return (
+    <div className="wrapper">{children}
+    </div>
+  );
+};
+
+export default Wrapper;
