5bf9e08a52655bf4c388a091c255368ce0e7fec4
Prasad W <88481022+MrPrasaD69@users.noreply.github.com>
2022-07-01 17:49:15 +0530

    Button File
    Button File added

---
+++ Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ className, value, onClick }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Button;
