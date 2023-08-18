13e54e73a2dfec0f260bb5db7060658fe01b9cc2
Prasad W <88481022+MrPrasaD69@users.noreply.github.com>
2023-08-18 11:10:37 +0530

    Functional Component Update
    Fetching Data with useEffect.

---
+++ web_app/client/src/GetData.js
@@ -0,0 +1,31 @@
+
+import React, {useState, useEffect} from "react";
+
+export default function GetData(){
+    const[post,getPost] = useState([]);
+    const API = "https://jsonplaceholder.typicode.com/posts";
+
+    const fetchPost=()=>{
+        fetch(API)
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res);
+            getPost(res);
+        })
+    }
+
+    useEffect(() => {
+        fetchPost()
+    }, [])
+
+    return(
+        <>
+            <h2>React Fetch Data with REST API Example</h2>
+            <ul>
+            {post.map((item, i) => {
+                return <li key={i}>{item.title}</li>
+            })}
+            </ul>
+        </>
+    )
+}
